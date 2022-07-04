d98ed36235271c2b406b5863399894b76e351c45
Arthur Carabott <arthur@arthurcarabott.com>
2022-07-03 19:25:27 -0700

    render state

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,9 +1,31 @@
+import { Atom, defAtom } from "@thi.ng/atom";
 import { start } from "@thi.ng/hdom";
-import { div } from "@thi.ng/hiccup-html";
+import { div, li, ul } from "@thi.ng/hiccup-html";
+import { map } from "@thi.ng/transducers";
+
+interface State {
+    values: number[];
+}
+
+interface Ctx {
+    state: Atom<State>;
+}
+
+const getModels = (ctx: Ctx) => {
+    ctx.state.deref().values;
+};
 
 const app = () => {
+    const stateAtom = defAtom<State>({ values: [666, 333] });
+
+    const ctx: Ctx = {
+        state: stateAtom,
+    };
+
     return () => {
-        return div({}, "hello");
+        const state = ctx.state.deref();
+        const valueCmps = map((value) => li({}, value), state.values);
+        return div({}, ul({}, valueCmps));
     };
 };
 
