94adc7be186b70fdea0af1b6be82758e370c2bb7
Arthur Carabott <arthur@arthurcarabott.com>
2022-07-03 19:21:53 -0700

    hello world

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,1 +1,10 @@
-console.log("hi")
+import { start } from "@thi.ng/hdom";
+import { div } from "@thi.ng/hiccup-html";
+
+const app = () => {
+    return () => {
+        return div({}, "hello");
+    };
+};
+
+start(app, { root: document.body });
